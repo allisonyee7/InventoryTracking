ffa8ab6d361cd8824764643016e541d4ccb44917
Allison <allisonyee56@gmail.com>
2017-02-23 20:58:24 -0500

    warehouse class completed

--- warehouse.py
+++ warehouse.py
@@ -19,20 +19,50 @@ class Bin:
         for item in self.contents:
             s += '\n  ' + item.__str__()
         return s
+class Warehouse:
+    """a location for storing Bins"""
+    def __init__(self, name):
+        self.name = name
+        self.bis = []
+    
+    def addbi(self, bi):
+        self.bis.append(bi)
 
-    def add(self, item):
-        for index, value in enumerate(self.contents):
+    def rmbi(self, bi):
+        self.bis.remove(bi)
+        
+    def add(self, bi, item):
+        for index, value in enumerate(bi.contents):
             if value.sku == item.sku:
                 value.quantity += item.quantity
                 break
             if value.sku < item.sku:
                 continue
             else:
-                self.contents.insert(index, item)
+                bi.contents.insert(index, item)
                 break
         else:
-            self.contents.append(item)
+            bi.contents.append(item)
 
+    def sell(self, bi, item):
+        for index, amount in enumerate(bi.contents):
+            if amount.sku == item.sku:
+                if amount.quantity - item.quantity == 0:
+                    bi.contents.remove(amount)
+                    break
+                elif amount.quantity - item.quantity < 0:
+                    print("You don't have that much in stock!\nTransaction Failed.")
+                    break
+                else:
+                    bi.contents.remove(amount)
+                    self.add(bi, BinItem(item.sku, amount.quantity - item.quantity))
+                    break
+
+    def __str__(self):
+        s = "Warehouse {0}:".format(self.name)
+        for bins in self.bis:
+            s += "\n  " + bins.__str__()
+        return s
 
 if __name__ == '__main__':
     import doctest
